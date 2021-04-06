8a1ea9264004e18f4e4a81750f12d82140a6e76b
Roy Adler <royadler@outlook.com>
2021-04-06 15:12:46 +0200

    adding ApiKey and SecretKey

--- Binance-Trader/BinanceTrader.cs
+++ Binance-Trader/BinanceTrader.cs
@@ -1,9 +1,23 @@
 ﻿using System;
 
-namespace Binance_Trader
-{
+namespace Binance
+
+{	
 		public class BinanceTrader
 		{
+				private string ApiKey;
+				private string SecretKey;
 
+				/// <summary>
+				/// Creates the main object to do all the api-calls.
+				/// 
+				/// </summary>
+				/// <param name="apiKey">Your apiKey from your Binance-Api.</param>
+				/// <param name="secretKey">Your secretKey from your Binance-Api.</param>
+				public BinanceTrader(string apiKey, string secretKey)
+				{
+						ApiKey = apiKey;
+						SecretKey = secretKey;
+				}
 		}
 }
